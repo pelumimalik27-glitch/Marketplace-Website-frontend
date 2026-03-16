136be91ff00786a64ac7263594ff206e72570327
pelumimalik4321 <pelumimalik27@gmail.com>
2026-03-16 07:50:47 +0100

    updated  code

--- src/Pages/AuthPage.jsx
+++ src/Pages/AuthPage.jsx
@@ -271,7 +271,7 @@ function AuthPage() {
                 }}
                 className='text-orange-600 text-sm'
               >
-              <p className="text-gray-800 mt-2  bg-orange-600 px-5 py-4">
+              <p className="text-gray-800 mt-2  bg-orange-400 px-4 py-3">
               {isAdminFlow
                 ? "Admin accounts can only sign in"
                 : isLogin
