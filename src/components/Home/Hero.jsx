d8ef60fe151e8f41753f3251cf1f1d14c733bfc4
pelumimalik4321 <pelumimalik27@gmail.com>
2026-03-18 09:56:40 +0100

    Describe what you changed

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
   const [active, setActive] = useState("");
 
   return (
-    <section className="relative w-full min-h-[320px] bg-[url('/images/bghero.jpg')] bg-cover lg:bg-contain bg-center py-16 sm:min-h-[380px] sm:py-24">
-      <div className="absolute inset-0 bg-black/60"></div>
-
+   <section className="relative w-full min-h-[320px] bg-[url('/images/new-hero.jpg')] bg-cover lg:bg-contain bg-center py-16 sm:min-h-[380px] sm:py-24">
+      <div className="absolute inset-0 bg-black/60 "></div>
+      
       <div className="relative z-10 flex h-full items-center justify-center text-white">
         <div className="max-w-xl px-4 text-center sm:px-6">
           <h1 className="text-2xl font-bold leading-tight sm:text-4xl md:text-5xl">Welcome to Elite Marketplace</h1>
